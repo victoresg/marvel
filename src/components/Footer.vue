4cb00b20c68c142fb6d165b678b428bbed18093f
victoresg <victor_esg9@hotmail.com>
2019-04-25 19:52:44 -0300

    Create new component footer.

---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="footer">
+    <ul class="footer-list">
+      <li>Home</li>
+      <li>Sobre</li>
+      <li>Trabalhe conosco</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style lang="scss">
+.footer {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &-list {
+    background-color: black;
+    display: flex;    
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    z-index: 999;
+    margin-bottom: 0rem;
+    li {
+      list-style-type: none;
+      color: white;
+      padding: 20px;
+    }
+  }
+}
+</style>
